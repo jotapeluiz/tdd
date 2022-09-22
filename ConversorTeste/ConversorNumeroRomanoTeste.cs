8b04b8daeeb755985b290dbe465e85d7029c8970
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2022-09-22 10:25:30 -0300

    Refatora os testes usando InlineData

--- ConversorTeste/ConversorNumeroRomanoTeste.cs
+++ ConversorTeste/ConversorNumeroRomanoTeste.cs
@@ -5,24 +5,20 @@ namespace ConversorTeste
 {
 	public class ConversorNumeroRomanoTeste
 	{
-		[Fact]
-		public void TesteDeveReconhecerSimboloI()
+		[Theory]
+		[InlineData("I", 1)]
+		[InlineData("V", 5)]
+		[InlineData("X", 10)]
+		[InlineData("L", 50)]
+		[InlineData("C", 100)]
+		[InlineData("D", 500)]
+		[InlineData("M", 1000)]
+		public void TesteDeveReconhecerUmSimbolo(string simboloTestado, int numeroEsperado)
 		{
 			var conversorNumeroRomano = new ConversorNumeroRomano();
-			var resultadoEsperado = 1;
-			var simboloConvertido = conversorNumeroRomano.ConververParaInteiro("I");
+			var numeroConvertido = conversorNumeroRomano.ConververParaInteiro(simboloTestado);
 
-			Assert.Equal(resultadoEsperado, simboloConvertido);
-		}
-
-		[Fact]
-		public void TesteDeveReconhecerSimboloV()
-		{
-			var conversorNumeroRomano = new ConversorNumeroRomano();
-			var resultadoEsperado = 5;
-			var simboloConvertido = conversorNumeroRomano.ConververParaInteiro("V");
-
-			Assert.Equal(resultadoEsperado, simboloConvertido);
+			Assert.Equal(numeroEsperado, numeroConvertido);
 		}
 	}
 }
