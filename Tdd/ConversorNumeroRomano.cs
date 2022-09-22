bc00e9a34ef741d0a1a337bffdb804ce3327046b
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2022-09-22 10:20:44 -0300

    Refatoração do método ConverterParaInteiro

--- Tdd/ConversorNumeroRomano.cs
+++ Tdd/ConversorNumeroRomano.cs
@@ -1,19 +1,30 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Tdd
 {
 	public class ConversorNumeroRomano
 	{
+		private Dictionary<string, int> SimbolosRomanos;
+
+		public ConversorNumeroRomano()
+		{
+			SimbolosRomanos = new Dictionary<string, int>
+			{
+				{ "I", 1 },
+				{ "V", 5 },
+				{ "X", 10 },
+				{ "L", 50 },
+				{ "C", 100 },
+				{ "D", 500 },
+				{ "M", 1000 }
+			};
+		}
+
 		public int ConververParaInteiro(string simbolo)
 		{
-			if (simbolo == "I")
+			if (SimbolosRomanos.ContainsKey(simbolo))
 			{
-				return 1;
-			}
-
-			if (simbolo == "V")
-			{
-				return 5;
+				return SimbolosRomanos[simbolo];
 			}
 
 			return 0;
