d087eb9758fbc1114f6d293efaf781021bbab07a
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2022-09-22 21:14:00 -0300

    Cria o teste para disparar exceção ao informar um valor negativo

--- VendasTeste/ProdutoTeste.cs
+++ VendasTeste/ProdutoTeste.cs
@@ -1,3 +1,4 @@
+using System;
 using Vendas;
 using Xunit;
 
@@ -16,6 +17,22 @@ namespace VendasTeste
 			};
 
 			Assert.Equal(50, produto.CalcularValorTotal());
+		}
+
+		[Theory]
+		[InlineData(10, -10)]
+		[InlineData(-10, 10)]
+		[InlineData(-10, -10)]
+		public void TesteDeveDispararExcecaoComValoresNegativos(int valorNegativo, int quantidadeNegativa)
+		{
+			var produto = new Produto
+			{
+				Nome = "Tesoura",
+				ValorUnitario = valorNegativo,
+				Quantidade = quantidadeNegativa
+			};
+
+			Assert.Throws<InvalidOperationException>(() => produto.CalcularValorTotal());
 		}
 	}
 }
