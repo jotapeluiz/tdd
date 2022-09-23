af46b4e48e369ce513aad7d2339192a074bf81e5
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2022-09-22 23:42:31 -0300

    Cria o teste onde verifica que o método não será executado com um cep inválido

--- VendasTeste/PedidoTeste.cs
+++ VendasTeste/PedidoTeste.cs
@@ -20,5 +20,21 @@ namespace VendasTeste
 
 			Assert.Equal(103, pedido.CalcularValorTotal(It.IsAny<string>(), It.IsAny<string>(), carrinhoDeCompras));
 		}
+
+		[Fact]
+		public void TesteNaoDeveCalcularOValorDoPedido()
+		{
+			var carrinhoDeCompras = new CarrinhoDeCompras();
+			carrinhoDeCompras.AdicionarNoCarrinho(new Produto { Nome = "Lapis", ValorUnitario = 3 });
+
+			var cepInvalido = string.Empty;
+			var mockServicoFrete = new Mock<IServicoFrete>();
+			var mockServicoCep = new Mock<IServicoCep>();
+
+			var pedido = new Pedido(mockServicoCep.Object, mockServicoFrete.Object);
+
+			pedido.CalcularValorTotal(cepInvalido, It.IsAny<string>(), carrinhoDeCompras);
+			mockServicoFrete.Verify(x => x.CalcularFrete(cepInvalido, It.IsAny<string>()), Times.Never());
+		}
 	}
 }
