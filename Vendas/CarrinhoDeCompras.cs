dd6e76f21db426f7db10c234a7097e370a789b0c
João Paulo Luiz Lopes <joaopaulo.luizlopes@outlook.com>
2022-09-22 21:40:41 -0300

    Cria classe Carrinho de Compras e seus respectivos cenários de testes

---
+++ Vendas/CarrinhoDeCompras.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Vendas
+{
+	public class CarrinhoDeCompras
+	{
+		private List<Produto> Produtos;
+
+		public CarrinhoDeCompras()
+		{
+			Produtos = new List<Produto>();
+		}
+
+		public void AdicionarNoCarrinho(Produto produto)
+		{
+			
+		}
+
+		public bool VerificarCarrinhoVazio()
+		{
+			return true;
+		}
+
+		public int CalcularValorCarrinho()
+		{
+			return 0;
+		}
+	}
+}
---
+++ VendasTeste/CarrinhoDeComprasTeste.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vendas;
+using Xunit;
+
+namespace VendasTeste
+{
+	public class CarrinhoDeComprasTeste
+	{
+		private CarrinhoDeCompras CarrinhoDeCompras;
+
+		public CarrinhoDeComprasTeste()
+		{
+			CarrinhoDeCompras = new CarrinhoDeCompras();
+		}
+
+		[Fact]
+		public void TesteDeveRetornarFalseSeCarrinhoEstiverVazio()
+		{
+			CarrinhoDeCompras.AdicionarNoCarrinho(new Produto { Nome = "Borracha", ValorUnitario = 4 });
+
+			Assert.False(CarrinhoDeCompras.VerificarCarrinhoVazio());
+		}
+
+		[Fact]
+		public void TesteRetornarZeroSeCarrinhoVazio()
+		{
+			Assert.Equal(0, CarrinhoDeCompras.CalcularValorCarrinho());
+		}
+
+		[Fact]
+		public void TesteSomarOsItensDoCarrinho()
+		{
+			CarrinhoDeCompras.AdicionarNoCarrinho(new Produto { Nome = "Lapis", ValorUnitario = 5 });
+			CarrinhoDeCompras.AdicionarNoCarrinho(new Produto { Nome = "Papel", ValorUnitario = 2, Quantidade = 20 });
+
+			Assert.Equal(45, CarrinhoDeCompras.CalcularValorCarrinho());
+		}
+	}
+}
